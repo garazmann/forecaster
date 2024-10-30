0745a1aaea2014415a93367f4769f3822667c310
garazmann <odpadak60@seznam.cz>
2024-10-30 21:11:35 +0100

    session

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,73 +2,109 @@
 
 namespace App\Controller;
 
+use App\Model\HoralApiDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/{treshold<\d+>?50}', host: 'api.localhost', name: 'app_apiweather_horalrika')]
-    public function indexApi(int $treshold): Response
+    #[Route('/weather/api', name: 'app_apiweather_horalrika')]
+    public function indexApi(
+        #[MapRequestPayload] ?HoralApiDTO $dto = null 
+    ): Response
     {
-        $draw = random_int(0, 100);
+        if (!$dto){
+            $dto = new HoralApiDTO();
+            $dto->treshold = 50;
+            $dto->trials = 1;
+        }
 
-        $forecast = $draw < $treshold ? 'Bude pršet' : 'Bude svítit sluníčko';
-
-        //return $this->render('weather/index.html.twig', [
-        //    'forecast' => $forecast]);
+        for ($i=0; $i < $dto->trials; $i++) { 
+            $draw = random_int(0, 100);
+            $forecast = $draw < $dto->treshold ? 'Bude pršet' : 'Bude svítit sluníčko';
+            $forecasts[] = $forecast;
+        }
 
         $json = [
-            'forecast' => $forecast,
-            'self' => $this->generateUrl(
-                'app_apiweather_horalrika',
-                 ['treshold' => $treshold],
-                 UrlGeneratorInterface::ABSOLUTE_URL
-                 )
+            'forecasts' => $forecasts,
+            'treshold' => $dto->treshold
         ];
 
         return new JsonResponse($json);
 
     }
 
-    #[Route('/weather/{treshold<\d+>?50}', name: 'app_weather_horalrika')]
-    public function index(int $treshold): Response
-    {
-        $draw = random_int(0, 100);
 
-        $forecast = $draw < $treshold ? 'Bude pršet' : 'Bude svítit sluníčko';
+
+    #[Route('/weather/{treshold<\d+>}', name: 'app_weather_horalrika')]
+    public function index(
+        Request $request,
+        RequestStack $requestStack,
+        ?int $treshold = null
+    ): Response
+    {
+        $session = $requestStack->getSession();
+        if ($treshold) {
+            $session->set('treshold', $treshold);
+        } else {
+            $treshold = $session->get('treshold', 50);
+        }
+
+        $trials = $request->get('trials', 1); 
+        $forecasts = [];
+
+        for ($i=0; $i < $trials; $i++) { 
+            $draw = random_int(0, 100);
+            $forecast = $draw < $treshold ? 'Bude pršet' : 'Bude svítit sluníčko';
+            $forecasts[] = $forecast;
+        }
 
         return $this->render('weather/index.html.twig', [
-            'forecast' => $forecast]);
+            'forecasts' => $forecasts,
+            'treshold' => $treshold
+        ]);
 
     }
 
 
 
-    // #[Route('/weather/{guess}')]
-    // public function indexGuess(string $guess): Response
-    // {
-    //     $draw = random_int(0, 100);
 
-    //     $forecast = "Dnes bude nejspíš počasí: $guess";
 
-    //     return $this->render('weather/index.html.twig', [
-    //         'forecast' => $forecast
-    //     ]);
-    // }
-
-    #[Route('/weather/{countryCode}/{city}')]
-    public function forecast(string $countryCode, string $city): Response
+    #[Route('/weather/{guess}')]
+    public function indexGuess(string $guess): Response
     {
-        
+
+        $avaiableGuesses = ['snezi', 'prsi', 'mrholi'];
+
+        if(!in_array($guess, $avaiableGuesses)){
+            throw $this->createNotFoundException('Tato možnost nenalezena.');
+        }
+
+        $forecast = "Dnes bude nejspíš počasí: $guess";
 
         return $this->render('weather/index.html.twig', [
-            'countryCode' => $countryCode,
-            'city' => $city
+            'forecasts' => [$forecast]
         ]);
     }
+
+    // #[Route('/weather/{countryCode}/{city}')]
+    // public function forecast(string $countryCode, string $city): Response
+    // {
+        
+
+    //     return $this->render('weather/index.html.twig', [
+    //         'countryCode' => $countryCode,
+    //         'city' => $city
+    //     ]);
+    // }
 
 
 
