ebb03d4fadc4f9615e8aa8d44b16c3146ea7a7a6
garazmann <odpadak60@seznam.cz>
2024-11-04 23:06:37 +0100

    forms

---
+++ src/Controller/LocationFormController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Location;
+use App\Form\LocationFormTestType;
+use App\Repository\LocationRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/location-form')]
+class LocationFormController extends AbstractController
+{
+    #[Route('/new', name: 'app_location_form')]
+    public function new(
+        Request $request,
+        LocationRepository $repository,
+        ): Response
+    {
+        $location = new Location();
+
+        $form = $this->createForm(LocationFormTestType::class, $location);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repository->save($location, true);
+
+            return $this->redirectToRoute('app_location_index');
+        }
+
+
+        return $this->render('location_form/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Form/LocationFormTestType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocationFormTestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('countryCode', ChoiceType::class, [
+                'choices' => [
+                    '' => null,
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Czechia' => 'CZ',
+                    'United Kingdom' => 'UK',
+                    'United States' => 'US',
+                ]
+            ])
+            ->add('latitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'min' => -90,
+                    'max' => 90,
+                ]
+            ])
+            ->add('longitude', NumberType::class, [
+                'html5' => true,
+                'scale' => 7,
+                'attr' => [
+                    'min' => -90,
+                    'max' => 90,
+                ]
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
